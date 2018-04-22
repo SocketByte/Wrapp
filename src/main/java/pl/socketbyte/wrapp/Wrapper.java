a87f5563137fb8bd6d5dc8c3be9e903b67af9225
SocketByte <poczta.xvacuum@gmail.com>
2018-04-22 13:54:41 +0200

    Map needs to be sorted :/

--- src/main/java/pl/socketbyte/wrapp/Wrapper.java
+++ src/main/java/pl/socketbyte/wrapp/Wrapper.java
@@ -2,12 +2,12 @@ package pl.socketbyte.wrapp;
 
 import java.io.Serializable;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 public class Wrapper<T> implements Serializable {
 
     private Class<? extends T> original;
-    private Map<String, FieldInfo> fields = new ConcurrentHashMap<>();
+    private Map<String, FieldInfo> fields = new ConcurrentSkipListMap<>();
 
     public Wrapper(Class<? extends T> original) {
         this.original = original;
