72ed974a112b2e3767b7f1e28cb39a9f79be395c
SocketByte <poczta.xvacuum@gmail.com>
2018-04-22 13:52:12 +0200

    Concurrency

--- src/main/java/pl/socketbyte/wrapp/Wrapper.java
+++ src/main/java/pl/socketbyte/wrapp/Wrapper.java
@@ -1,13 +1,13 @@
 package pl.socketbyte.wrapp;
 
 import java.io.Serializable;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Wrapper<T> implements Serializable {
 
     private Class<? extends T> original;
-    private Map<String, FieldInfo> fields = new HashMap<>();
+    private Map<String, FieldInfo> fields = new ConcurrentHashMap<>();
 
     public Wrapper(Class<? extends T> original) {
         this.original = original;
